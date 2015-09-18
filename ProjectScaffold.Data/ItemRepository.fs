f2d9c33cf5367caab619f8abaf480e506251814a
Leif Battermann <leif.battermann@webrunners.de>
2015-09-18 12:58:02 +0200

    create context from within the repo

--- ProjectScaffold.Data/ItemRepository.fs
+++ ProjectScaffold.Data/ItemRepository.fs
@@ -10,30 +10,32 @@ module AccessLayer =
     open Chessie.ErrorHandling
 
     type dbSchema = SqlDataConnection<"Data Source=(localdb)\V11.0;Initial Catalog=ProjectScaffold.Database;Integrated Security=SSPI;">
-    let private db = dbSchema.GetDataContext()
-
-    // Enable the logging of database activity to the console.
-    db.DataContext.Log <- System.Console.Out
-
-    let private descriptionFrom =
-        function
-        | Some v -> v
-        | _      -> null // is null really a good idea? why not empty?
-
-    let private fromItemDto (dto: dbSchema.ServiceTypes.Item): Item =
-        { id = ItemId dto.Id; name = dto.Name; description = if dto.Description <> null then Some dto.Description else None }
-
-    let private toItemDto (ItemId id, description, name) =
-        dbSchema.ServiceTypes.Item(Id = id, Description = descriptionFrom description, Name = name)
-
-    let private tryFindItemDto (ItemId itemId) =
-        db.Item
-        |> Seq.tryFind (fun dto -> dto.Id = itemId) 
-
-    let private tryFindItem =
-        tryFindItemDto >> Option.map fromItemDto
 
     type ItemRepository() =
+
+        let db = 
+            let db = dbSchema.GetDataContext()
+            // Enable the logging of database activity to the console.
+            db.DataContext.Log <- System.Console.Out
+            db
+
+        let descriptionFrom =
+            function
+            | Some v -> v
+            | _      -> null
+
+        let fromItemDto (dto: dbSchema.ServiceTypes.Item): Item =
+            { id = ItemId dto.Id; name = dto.Name; description = if dto.Description <> null then Some dto.Description else None }
+
+        let toItemDto (ItemId id, description, name) =
+            dbSchema.ServiceTypes.Item(Id = id, Description = descriptionFrom description, Name = name)
+
+        let tryFindItemDto (ItemId itemId) =
+            db.Item
+            |> Seq.tryFind (fun dto -> dto.Id = itemId) 
+
+        let tryFindItem =
+            tryFindItemDto >> Option.map fromItemDto
 
         interface IItemReadAccess with
             
@@ -74,4 +76,5 @@ module AccessLayer =
                             ok()
                         | _ -> fail (ItemNotFound)
                 with
-                | ex -> fail (DbUpdateError ex.Message)
+                | ex -> 
+                    fail (DbUpdateError ex.Message)
